d80d8e3976b858a0402fbe3ec889f7609bb2465c
Fariyal0218 <fariyalshaikh5@gmail.com>
2021-12-22 23:08:36 +0530

    first commit

---
+++ Javascript/js-intro.js
@@ -0,0 +1,56 @@
+//alert("Welcome to JS")
+
+//3 keywords for declaring a variable in js
+
+//var,let,const
+
+//var keyword variable are global scope variable as well as function scope variable .
+//let keyword variable are block scope variable as well as function scope variable.
+//const keyword variable for a constant value variable.
+
+
+//hoisting-whatever declaration part of variable as well as there that is hoisted at the top 
+
+//num1=20;
+//console.log(num1)
+//var num1;
+
+let num1;
+num1=258
+console.log(num1)
+
+var fname="codekul"
+
+num1=35;
+
+num5=55;
+console.log(num5)
+
+let num5;
+
+//console.log(city)
+console.log(fname)
+
+if (true){
+    console.log(num1)
+    console.log(city)
+    var num2=100;
+
+    console.log(num2)
+
+    console.log(fname)
+}
+
+function displayNum(){
+    //var fname="codekul"
+
+    let city ="Pune"
+    var num3=150;
+    console.log(num3)
+}
+
+displayNum()
+console.log(num2)
+//console.log(num3)
+
+console.log(fname)
